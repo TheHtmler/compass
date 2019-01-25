50ad4cdc9eab6b5a3f975f7ae0eddf812f8a5e17
13167144020 <wangleileo08@163.com>
2019-01-25 17:37:05 +0800

    add ico

--- Web_UI/build/webpack.dev.conf.js
+++ Web_UI/build/webpack.dev.conf.js
@@ -55,6 +55,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
+      favicon: 'compass.ico',
       inject: true
     }),
     // copy custom static assets
